c7e9a52126cac3c1d380ef58617b5431754144fe
mirzoibodullaev <1bodullaevm11@gmail.com>
2023-11-07 21:08:51 +0500

    add BundleAnalayzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,8 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
-import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import { BuildOptions } from "./types/config";
 
 export function buildPlugins({
     paths,
@@ -18,6 +19,9 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ];
 }
