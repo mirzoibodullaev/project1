fe131f38e1da494e551ccce7f519922c80b27da4
mirzoibodullaev <1bodullaevm11@gmail.com>
2023-11-07 20:48:10 +0500

    add ErrorBoundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,12 +3,7 @@ import { useTranslation } from "react-i18next";
 
 const MainPage = () => {
     const { t } = useTranslation("main");
-    return (
-        <div>
-            <BugButton />
-            {t("Главная страница")}
-        </div>
-    );
+    return <div>{t("Главная страница")}</div>;
 };
 
 export default MainPage;
